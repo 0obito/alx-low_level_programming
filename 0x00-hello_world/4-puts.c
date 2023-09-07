5bb6eefc3ebda61eaa57221515a1adc70e80c382
0obito <elmsafer.abdellah00@gmail.com>
2023-09-07 12:21:47 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Prints a sentence starting with a double-quote
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
