6c86eb4c1ba45d2086d9a2d8c27cccbebad95e76
0obito <elmsafer.abdellah00@gmail.com>
2023-09-07 11:58:57 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
-int function()
+#include <stdio.h>
+
+int main()
 {
-  puts ("Programming is like building a multilingual puzzle",\n);
+  puts ("Programming is like building a multilingual puzzle");
+  return 0;
 }
-return(0)
