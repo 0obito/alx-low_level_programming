1259d37ba2af70e2715c34c157ea4c20597a1bef
0obito <elmsafer.abdellah00@gmail.com>
2023-09-08 11:12:46 -0700

    tsak 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
- * Description: prints the alphabet in lower case
+ * Description: Prints the alphabet in lower case
  *
- * return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
