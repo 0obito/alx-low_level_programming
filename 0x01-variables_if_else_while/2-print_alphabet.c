646e634706c404b20b36efe2603eef34fa4e523f
0obito <elmsafer.abdellah00@gmail.com>
2023-09-08 10:56:29 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lower case
+ *
+ * Return : Always 0 (Success)
+ */
+int main(void)
+{
+char alpha = 'a'
+while (alpha <= 'z'){
+	putchar(alpha);
+	alpha++;
+}
+putchar('\n');
+}
