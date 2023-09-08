3550896a67ea9adb28c84d1a6fdf620e75d5c249
0obito <elmsafer.abdellah00@gmail.com>
2023-09-08 13:02:48 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the numbers from 0 to 9 using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+char ascii = '0'
+while (num < 10)
+{
+	putchar('0' + num);
+	num++;
+}
+printf("\n");
+return (0);
+}
