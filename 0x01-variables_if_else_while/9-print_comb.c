2ff7d2345647fa5459a7c3bd13ec02e2d9f9654a
0obito <elmsafer.abdellah00@gmail.com>
2023-09-08 13:45:57 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+	putchar('0' + num);
+	putchar(',');
+	if (num == 9)
+		break;
+	else
+		putchar(' ');
+	num++;
+}
+putchar('\n');
+return (0);
+}
