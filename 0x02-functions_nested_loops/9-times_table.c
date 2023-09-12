b982438266843d83d6d363ac182fd9284d8715c4
0obito <elmsafer.abdellah00@gmail.com>
2023-09-12 16:18:58 -0700

    09

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -30,7 +30,7 @@ void times_table(void)
 		    _putchar('0' + (result / 10));
 		    _putchar('0' + (result % 10));
             }
-	    if (b > 9)
+	    while (b < 9)
 		    _putchar(',');
         }
         _putchar('\n');
