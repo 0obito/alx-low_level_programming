9b6c53f3ce940886076f6744500b7a9f13831f8d
0obito <elmsafer.abdellah00@gmail.com>
2023-09-12 13:29:47 -0700

    06

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,11 +5,13 @@
  * _abs - Entry point
  *
  * Description: computes absolute value of an integer
+ *
+ * Return: absolute value
  */
 int _abs(int n)
 {
 if (n < 0)
 	return (-n);
 else
-        return (n);
+	return (n);
 }
