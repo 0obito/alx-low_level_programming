db65e544e261e2c9252a4361793e36b1d7c8ca3b
0obito <elmsafer.abdellah00@gmail.com>
2023-09-12 16:33:46 -0700

    0909

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,35 +8,34 @@
  */
 void times_table(void)
 {
-    int a;
-    int b;
-    for (a = 0; a <= 9; a++)
-    {
-        for (b = 0; b <= 9; b++)
-        {
-            int result = a * b;
-	    if (b == 0)
-	    {
-		    _putchar('0' + result);
-	    }
-	    else if (b > 0 && result < 10)
-            {
-		    _putchar(' ');
-		    _putchar(' ');
-		    _putchar('0' + result);
-            }
-            else if (b > 0 && result >= 10)
-            {
-		    _putchar(' ');
-		    _putchar('0' + (result / 10));
-		    _putchar('0' + (result % 10));
-            }
-
-	    if (b < 9)
-	    {
-		    _putchar(',');
-	    }
-        }
-        _putchar('\n');
-    }
+int a;
+int b;
+for (a = 0; a <= 9; a++)
+{
+	for (b = 0; b <= 9; b++)
+	{
+		int result = a * b;
+		if (b == 0)
+		{
+			_putchar('0' + result);
+		}
+		else if (b > 0 && result < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + result);
+		}
+		else if (b > 0 && result >= 10)
+		{
+			_putchar(' ');
+			_putchar('0' + (result / 10));
+			_putchar('0' + (result % 10));
+		}
+		if (b < 9)
+		{
+			_putchar(',');
+		}
+	}
+	_putchar('\n');
+}
 }
