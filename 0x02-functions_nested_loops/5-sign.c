dd84fa8ee7751b8c8f0f7a046f9a3a05fe33cf87
0obito <elmsafer.abdellah00@gmail.com>
2023-09-12 13:04:57 -0700

    05

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,21 +1,30 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ * print_sign - Entry function
+ *
+ * Description: Prints the sign of a number
+ * 
+ * @n: the number
+ *
+ * Return: 1 if +, 0 if 0, -1 if -
+ */
 int print_sign(int n)
 {
 if (n > 0)
 {
 	_putchar('+');
-	return(1);
+	return (1);
 }
-else if (n==0)
+else if (n == 0)
 {
 	_putchar('0');
-	return(0);
+	return (0);
 }
 else
 {
 	_putchar('-');
-	return(-1);
+	return (-1);
 }
 }
