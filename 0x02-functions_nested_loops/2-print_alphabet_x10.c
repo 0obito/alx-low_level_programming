6d057a103613e8ecbee17c68a7c6d949fdd87cac
0obito <elmsafer.abdellah00@gmail.com>
2023-09-12 08:22:18 -0700

    02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+# include "main.h"
+# include <unistd.h>
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: Prints 10 times the alphabet in lowercase
+ *
+ * Return: Void (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char alpha = 'a';
+	int i = 0;
+while (i < 10)
+{
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+	i++;
+}
+}
