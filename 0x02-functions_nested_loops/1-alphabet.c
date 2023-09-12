e0343da9dbbe8e22c64d2e1b6e5c24ab94324ee8
0obito <elmsafer.abdellah00@gmail.com>
2023-09-12 07:00:59 -0700

    01

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+# include <unistd.h>
+
+/**
+ * main- Entry point
+ *
+ * Description: prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char alpha='a';
+while(alpha<'z')
+{
+	_putchar(alpha);
+	alpha++;
+}
+_putchar('\n');
+return(0);
+}
