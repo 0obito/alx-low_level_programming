4dcce08e2ec965aede228dde6381b6cef2c84e09
0obito <elmsafer.abdellah00@gmail.com>
2023-09-12 16:41:31 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ *
+ * Description: adds two integers
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: returns the result
+ */
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
