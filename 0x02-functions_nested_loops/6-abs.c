18593ae618adb5740d72f7803126a3487e183781
0obito <elmsafer.abdellah00@gmail.com>
2023-09-12 13:17:41 -0700

    06

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * Description: computes absolute value of an integer
+ */
+int _abs(int)
+{
+int n;
+if (n < 0)
+	n == -n;
+else
+	break;
+_putchar(n);
+}
