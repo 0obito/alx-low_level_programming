09d7f97631a048d72a98e94458463434766890f3
0obito <elmsafer.abdellah00@gmail.com>
2023-09-14 13:55:01 -0700

    05

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,7 +16,15 @@ void more_numbers(void)
 		j = 0;
 		while (j <= 14)
 		{
-			_putchar(j + '0');
+			if (j < 10)
+			{
+				_putchar(j + '0');
+			}
+			else
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
 			j++;
 		}
 		_putchar('\n');
