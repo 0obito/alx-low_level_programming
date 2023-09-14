bb4e543afead35bbcea357f6e0bfe22c38020e75
0obito <elmsafer.abdellah00@gmail.com>
2023-09-14 15:24:23 -0700

    oooooooy

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Fizz-Buzz Test
+ *
+ * Return: 0
+ */
+int main()
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		while (i < 100)
+			printf(" ");
+	}
+}
