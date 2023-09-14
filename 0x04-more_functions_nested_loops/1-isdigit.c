1c99eb82d9efd68c34e2d9635f9cbada60d82238
0obito <elmsafer.abdellah00@gmail.com>
2023-09-14 12:33:40 -0700

    01

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ *
+ * Description: checks for a digit
+ *
+ * @c: checking character
+ *
+ * Return: 1 if a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
