1cf36800c08903ee224d85edabe207d071fc9940
0obito <elmsafer.abdellah00@gmail.com>
2023-09-14 16:23:35 -0700

    last, maybe

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ *
+ * Description: draws a triangle
+ *
+ * @size: size of triangle
+ */
+void print_triangle(int size);
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 1; i <= size; i++)
+	{
+		j = size;
+		while (j > i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		k = 0;
+		while (k < i)
+		{
+			_putchar('#');
+			k++;
+		}
+		_putchar('\n');
+	}
+
+	if (size <= 0)
+		_putchar('\n');
+}
