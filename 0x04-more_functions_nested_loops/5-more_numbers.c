361b71232a3742ef7eb3a317b55889acf6eabe53
0obito <elmsafer.abdellah00@gmail.com>
2023-09-14 13:38:02 -0700

    05

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Description: prints numbers from 0 to 14, 10 times
+ */
+void more_numbers(void);
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(i + '0');
+		}
+		j = 0;
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
