99bff8bf4cb9089dd2baffc563fbbcf7c3cf3201
0obito <elmsafer.abdellah00@gmail.com>
2023-09-18 18:16:07 -0700

    opa

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ *
+ * Description: computes the length of a string
+ *
+ * @s: the string
+ *
+ * Return: returns the length of s
+ */
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+  length++;
+  *s++;
+}
+return(length);
+}
