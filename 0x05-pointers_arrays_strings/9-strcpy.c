246304ccf42d0fbf2d6b6065235c3be3a36a078a
0obito <elmsafer.abdellah00@gmail.com>
2023-09-19 07:00:18 -0700

    zombie

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Entry point
+ *
+ * Decription: copies the string pointed
+ * to, to the buffer pointed to
+ *
+ * @dest: a buffer
+ *
+ * @src: a string
+ *
+ * Return: *dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 1;
+int tab[1000];
+
+while (*src != '\0')
+{
+tab[i] = *src;
+i++;
+src++;
+}
+src--;
+i = 1;
+
+while (*src >= 1)
+{
+*dest = tab[i];
+i++;
+dest++;
+src--;
+}
+
+printf("\n");
+}
