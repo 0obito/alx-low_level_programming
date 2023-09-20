5a7d34bc0185ebb144a01783190c79cc0d46853e
0obito <elmsafer.abdellah00@gmail.com>
2023-09-20 04:48:54 -0700

    ops

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - entry point
+ * description: concatenates 2 strings
+ * @dest: destination string
+ * @src: source string
+ * return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+if (*dest == '\0')
+break;
+else
+{
+do
+{
+dest++;
+}
+while (*dest != '\0');
+}
+
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+return (dest);
