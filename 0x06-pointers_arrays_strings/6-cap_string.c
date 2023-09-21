23062913b01689faaf12de2889b45fc634ecf5f1
0obito <elmsafer.abdellah00@gmail.com>
2023-09-21 04:28:38 -0700

    olazomi

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * cap_string - entry point
+ *
+ * @s: the string
+ *
+ * Description: Capitalizes all words of a string.
+ *
+ * Return: A pointer to the Capitalized string.
+ */
+char *cap_string(char *s)
+{
+char *z = s;
+
+if (*s >= 'a' && *s <= 'z')
+{
+*s = *s - 32;
+s++;
+}
+
+while (*s != '\0')
+{
+
+if (&s == (' ' || '\t' || '\n' || ',' || ';' || '.' || '!' || '?' || '"' || '(' || ')' || '{' || '}'))
+{
+s++;
+if (*s >= 'a' && *s <= 'z')
+{
+*s = *s - 32;
+s++;
+}
+}
+
+else
+{
+s++;
+}
+
+}
+return (z);
+}
