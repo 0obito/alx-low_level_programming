25df9ce40704b60015c4d5c04953cb6e694f4f03
0obito <elmsafer.abdellah00@gmail.com>
2023-09-21 08:24:43 -0700

    olazomi

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,10 +7,11 @@
  *
  * Description: Encodes a string into leet system.
  *
- * Return: The leet coded string 'leet'.
+ * Return: The leet coded string 'aelot'.
  */
 char *leet(char *s)
 {
+int i;
 char *aelot = s;
 char t[5] = "43107";
 char t1[5] = "AELOT";
@@ -19,10 +20,12 @@ char t2[5] = "aelot";
 while (*s != '\0')
 {
 
-for (int i = 0; i < 5; i++)
+for (i = 0; i < 5; i++)
 {
 if (*s == t1[i] || *s == t2[i])
+{
 *s = t[i];
+}
 }
 
 s++;
