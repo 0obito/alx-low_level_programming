d6d2e7092dac463312dcd2e9d77987c82fce7871
0obito <elmsafer.abdellah00@gmail.com>
2023-09-25 23:18:47 +0100

    A

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * - Entry point
+ *
+ * @s: the pointer
+ * @b: the constant byte
+ * @n: number of bytes pointed to
+ *
+ * Description:  Fills the memory with a constant byte.
+ *
+ * Return: A pointer to the memory area s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+char *p = s;
+
+for (; n > 0; n--)
+{
+	*s = b;
+	s++;
+}
+return (p);
+}
