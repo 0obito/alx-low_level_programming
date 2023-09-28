04fdd67be7faaa83d49980acd762450dabe0bb20
0obito <elmsafer.abdellah00@gmail.com>
2023-09-28 23:04:55 +0100

    A

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,14 @@
  * @s: Points to a string.
  */
 void _puts_recursion(char *s)
-{	
+{
 	_putchar(*s);
-	
+
 	if (*s != '\0')
+	{
+		_putchar(*s);
 		_puts_recursion(s + 1);
-	
+	}
 	if (*s == '\0')
 		_putchar('\n');
 }
