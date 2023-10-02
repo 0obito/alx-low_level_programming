58ec7520a151794c8421e22335f651061720e811
0obito <elmsafer.abdellah00@gmail.com>
2023-10-02 11:00:34 -0700

    ooasla

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 #!/bin/bash
-for source_file in *.c
-do
-  object_file="${source_file%.c}.o"
-  gcc -c "$source_file" -o "$object_file"
-done
-gcc -c *.c -o *.o
+gcc *.c
 ar rcs liball.a *.o
