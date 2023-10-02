42d0283a208b6598d66b62fa9f3b61b36a8fcb54
0obito <elmsafer.abdellah00@gmail.com>
2023-10-02 07:06:07 -0700

    ohayo

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * int _atoi(char *s) - Entry point
+ *
+ * Description: it's actually empty.
+ *
+ * Return: Nothing mate.
+ */
+int _atoi(char *s)
+{
+return(0);
+}
