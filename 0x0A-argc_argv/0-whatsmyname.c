2403da75e62cc05d54ae538f5fe4b70ff6cea1bf
0obito <elmsafer.abdellah00@gmail.com>
2023-10-02 19:35:39 +0100

    A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the first string on the command line.
+ *
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return(0);
+}
