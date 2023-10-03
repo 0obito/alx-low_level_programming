f2639addeb4abb088eee7a25c051142fbff3c343
0obito <elmsafer.abdellah00@gmail.com>
2023-10-03 16:45:01 +0100

    A

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
 	char *ptr;
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(size);
 
-	for (i = 0; i < size)
+	for (i = 0; i < size; i++)
 	{
 	ptr[i] = c;
 	}
