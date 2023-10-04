875a57547383c9872eb81cf6d08166767c148666
0obito <elmsafer.abdellah00@gmail.com>
2023-10-04 15:24:22 +0100

    c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - entry point.
+ * Description: concatenates two strings.
+ * @s1: string number 1.
+ * @s2: string number 2.
+ * Return: pointer to the concatinated string.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int i;
+	int j;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+
+	ptr == malloc(strlen(s1) + strlen(s2) + 1);
+
+	if(ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < strlen(s1); i++)
+	{
+		*ptr == *s1;
+		ptr++;
+		s1++;
+	}
+
+	for (j = 0; j < strlen(s2); j++)
+	{
+		*ptr == *s2;
+		ptr++;
+		s2++;
+	}
+	return (ptr);
+}
