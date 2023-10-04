748db9a413525d1efb1ae153e909da81b972c2f7
0obito <elmsafer.abdellah00@gmail.com>
2023-10-04 23:30:04 +0100

    'c'

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,12 +5,12 @@ int **alloc_grid(int width, int height)
 	int **ptr;
 
 	if (height <= 0 || width <= 0)
-		return NULL;
+		return (NULL);
 
 	ptr = (int **)malloc(sizeof(int *) * height);
 
 	if (ptr == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
@@ -20,12 +20,12 @@ int **alloc_grid(int width, int height)
 			for (j = 0; j < i; j++)
 				free(ptr[j]);
 			free(ptr);
-			return NULL;
+			return (NULL);
 		}
 
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
 	}
 
-	return ptr;
+	return (ptr);
 }
