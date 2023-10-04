fc6c0ee3a6e3ad6e7539e59dd3618d9d383e6b0e
0obito <elmsafer.abdellah00@gmail.com>
2023-10-05 00:48:26 +0100

    'c'

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,18 +13,16 @@ char *argstostr(int ac, char **av)
 {
 	int i;
 	int j;
-	int length;
+	int length = 0;
 	char *ptr;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-		length = length + strlen(av[i]);
+		length += strlen(av[i]) + 1;
 
-	length = length + ac;
-
-	ptr = (char *)malloc(length);
+	ptr = (char *)malloc(length + 1);
 
 	if (ptr == NULL)
 		return (NULL);
@@ -33,7 +31,7 @@ char *argstostr(int ac, char **av)
 
 	for (j = 0; j < ac; j++)
 	{
-		strcat(ptr, av[i]);
+		strcat(ptr, av[j]);
 
 		if(i < ac - 1)
 			strcat(ptr, "\n");
