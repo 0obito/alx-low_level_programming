7acdc758138ec06bd8276a44197857024c62fb90
0obito <elmsafer.abdellah00@gmail.com>
2023-10-06 17:23:45 +0100

    'Cc'

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,5 +16,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *array;
+	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	array = malloc(size * nmemb);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		array[i] = 0;
+	}
+
+	return (array);
 }
