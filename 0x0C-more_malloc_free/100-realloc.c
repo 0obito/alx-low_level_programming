75b63e2ac7612dfb9812164c5b04d66343df9486
0obito <elmsafer.abdellah00@gmail.com>
2023-10-06 18:30:27 +0100

    'E_for_Extra'

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - entry point
+ *
+ * @ptr: a pointer to the memory previously allocated.
+ * @old_size: the size of ptr.
+ * @new_size: a new size for the new pointer.
+ *
+ * Description: reallocates a memory block using
+ * malloc and free.
+ *
+ * Return: a pointer to a newly
+ * allocated space in the memory.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+
+}
