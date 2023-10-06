d01e2be68428a4193f32d85d35bcb80c8b9b59c6
0obito <elmsafer.abdellah00@gmail.com>
2023-10-06 09:00:09 -0700

    Cc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - entry point
+ *
+ * @nmemb: number of elements.
+ * @size: size of array's elements.
+ *
+ * Description: allocates memory for an array
+ * using malloc.
+ *
+ * Return: a pointer to an array
+ * of nmemb elements of size bytes each.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+}
