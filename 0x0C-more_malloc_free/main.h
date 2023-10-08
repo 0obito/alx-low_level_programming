0aa4e7fd70d20b2c43ceba6f7f97a280fce14041
0obito <elmsafer.abdellah00@gmail.com>
2023-10-08 02:10:31 +0100

    'E_for_Extra'

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,12 @@
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+char *strcat_bothnull(char *s1, char *s2, unsigned int n);
+char *strcat_onenull(char *s1, char *s2, unsigned int n);
+char *strcat_twonull(char *s1, char *s2, unsigned int n);
+char *strcat_nonenull(char *s1, char *s2, unsigned int n);
+
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
