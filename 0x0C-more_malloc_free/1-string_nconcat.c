fe1bf11f0c4eed585342ccb26054b42ecc1d9fe4
0obito <elmsafer.abdellah00@gmail.com>
2023-10-08 20:50:55 +0100

    'E_for_Extra'

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,20 +8,21 @@
  *
  * Return: filled string.
  */
-char *strcat_both(char *str){
+char *strcat_both(char *str)
+{
 	if (str == NULL)
 		return (NULL);
 	str[0] = '\0';
 	return (str);
 }
-
 /**
  * strcat_two - fills the string if
  * string s2 is NULL
  *
  * Return: filled string.
  */
-char *strcat_two(char *str, char *s1) {
+char *strcat_two(char *str, char *s1)
+{
 	unsigned int i;
 
 	if (str == NULL)
@@ -31,14 +32,14 @@ char *strcat_two(char *str, char *s1) {
 	str[i] = '\0';
 	return (str);
 }
-
 /**
  * strcat_one - fills the string if
  * string s1 is NULL
  *
  * Return: filled string.
  */
-char *strcat_one(char *str, char *s2, unsigned int size) {
+char *strcat_one(char *str, char *s2, unsigned int size)
+{
 	unsigned int i;
 
 	if (str == NULL)
@@ -48,14 +49,14 @@ char *strcat_one(char *str, char *s2, unsigned int size) {
 	str[i] = '\0';
 	return (str);
 }
-
 /**
  * strcat_none - fills the string if
  * neither of the strings is NULL
  *
  * Return: filled string.
  */
-char *strcat_none(char *str, char *s1, char *s2, unsigned int size) {
+char *strcat_none(char *str, char *s1, char *s2, unsigned int size)
+{
 	unsigned int i;
 	unsigned int j;
 
@@ -68,7 +69,6 @@ char *strcat_none(char *str, char *s1, char *s2, unsigned int size) {
 	str[i + j] = '\0';
 	return (str);
 }
-
 /**
  * string_nconcat - entry point
  * @s1: string one.
@@ -77,33 +77,41 @@ char *strcat_none(char *str, char *s1, char *s2, unsigned int size) {
  * Description: concatenates two strings.
  * Return: a pointer to the concatenated string.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n) {
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
 	char *str, *both, *two, *ones, *onen, *nonen, *nones;
 
-	if (s1 == NULL && s2 == NULL) {
+	if (s1 == NULL && s2 == NULL)
+	{
 		str = malloc(sizeof(*str) * 1);
 		both = strcat_both(str);
 		return (both);
 	}
-	else if (s1 == NULL || s2 == NULL) {
-		if (s2 == NULL) {
+	else if (s1 == NULL || s2 == NULL)
+	{
+		if (s2 == NULL)
+		{
 			str = malloc(sizeof(*str) * (strlen(s1) + 1));
 			two = strcat_two(str, s1);
 			return (two);
 		}
-		if (s1 == NULL && n >= strlen(s2)) {
+		if (s1 == NULL && n >= strlen(s2))
+		{
 			str = malloc(sizeof(*str) * (strlen(s2) + 1));
 			ones = strcat_one(str, s2, strlen(s2));
 			return (ones);
 		}
-		if (s1 == NULL && n < strlen(s2)) {
+		if (s1 == NULL && n < strlen(s2))
+		{
 			str = malloc(sizeof(*str) * (n + 1));
 			onen = strcat_one(str, s2, n);
 			return (onen);
 		}
 	}
-	else {
-		if (n >= strlen(s2)) {
+	else
+	{
+		if (n >= strlen(s2))
+		{
 			str = malloc(sizeof(*str) * (strlen(s1) + strlen(s2) + 1));
 			nones = strcat_none(str, s1, s2, strlen(s2));
 			return (nones);
