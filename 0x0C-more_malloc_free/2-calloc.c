dbe2205a22786cf7dc3554654f25121593388b6b
0obito <elmsafer.abdellah00@gmail.com>
2023-10-08 21:13:14 +0100

    'E_for_Extra'

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,10 +16,10 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *array;
+	void *array;
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
 	array = malloc(size * nmemb);
