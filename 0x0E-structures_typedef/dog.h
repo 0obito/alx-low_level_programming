734fe81c13c3459844289e3529bedf83df9dea9b
0obito <elmsafer.abdellah00@gmail.com>
2023-10-10 21:54:40 +0100

    'Bingo'

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * This structure represents information about a dog, including its name, age, and owner.
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 #endif /* DOG_H */
