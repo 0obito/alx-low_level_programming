146e20228d21f2adc4aa7d1cae44427fff1e5dc9
0obito <elmsafer.abdellah00@gmail.com>
2023-10-10 22:12:49 +0100

    'Bingo'

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * init_dog - entry point
+ *
+ * *d: pointer to the dog.
+ * *name: pointer to its name.
+ * age: its age.
+ * *owner: pointer to its age.
+ *
+ * Description: initializes a dog with
+ * a name, an age and an owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = {name, age, owner};
+}
