adb10d467a80bdff66641fc41ec66266a81b0902
0obito <elmsafer.abdellah00@gmail.com>
2023-10-11 01:10:27 +0100

    'Bingo'

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,22 +13,24 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		struct dog *dog_ptr = malloc(sizeof(struct dog));
+
 		if (dog_ptr == NULL)
 			exit(EXIT_FAILURE);
-	dog_ptr->name = (d->name != NULL) ? d->name : NULL;
-	dog_ptr->age = d->age;
-	dog_ptr->owner = (d->owner != NULL) ? d->owner : NULL;
 
-	if (dog_ptr->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", dog_ptr->name);
+		dog_ptr->name = (d->name != NULL) ? d->name : NULL;
+		dog_ptr->age = d->age;
+		dog_ptr->owner = (d->owner != NULL) ? d->owner : NULL;
 
-	printf("Age: %f\n", dog_ptr->age);
+		if (dog_ptr->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", dog_ptr->name);
 
-	if (dog_ptr->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", dog_ptr->owner);
+		printf("Age: %f\n", dog_ptr->age);
+
+		if (dog_ptr->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", dog_ptr->owner);
 	}
 }
