9183586a90ff4e053e163b1ee3a8f3e83645f0f2
0obito <elmsafer.abdellah00@gmail.com>
2023-10-11 15:26:15 +0100

    'olazomi'

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 	if (array == NULL || cmp == NULL)
-		return (NULL);
+		return (-1);
 	i = 0;
 	while (i < size)
 	{
