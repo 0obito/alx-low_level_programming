a487225db64a49ee9ae7bc750cc03e91ca0dd6e9
0obito <elmsafer.abdellah00@gmail.com>
2023-10-10 17:00:04 -0700

    djdj

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
 /**
@@ -6,20 +8,28 @@
  *
  * @d: data structure of a dog.
  */
-void print_dog(struct dog *d)
-{
-	if (d != NULL)
+void print_dog(struct dog *d) {
+    if (d != NULL) {
+        struct dog *dog_ptr = malloc(sizeof(struct dog));
+        if (dog_ptr == NULL)
 	{
-		if ((*d).name == NULL)
-			printf("Name: (nil)");
-		else
-			printf("Name: %s", (*d).name);
-		printf("\n");
-		printf("Age: %f", (*d).age);
-		printf("\n");
-		if ((*d).owner == NULL)
-			printf("Owner: (nil)");
-		else
-			printf("Owner: %s", (*d).owner);
+	exit(1);
 	}
+        
+        dog_ptr->name = (d->name != NULL) ? d->name : NULL;
+        dog_ptr->age = d->age;
+        dog_ptr->owner = (d->owner != NULL) ? d->owner : NULL;
+
+        if (dog_ptr->name == NULL)
+            printf("Name: (nil)\n");
+        else
+            printf("Name: %s\n", dog_ptr->name);
+
+        printf("Age: %f\n", dog_ptr->age);
+
+        if (dog_ptr->owner == NULL)
+            printf("Owner: (nil)\n");
+        else
+            printf("Owner: %s\n", dog_ptr->owner);
+    }
 }
