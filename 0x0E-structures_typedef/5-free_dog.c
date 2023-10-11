a8e91646e75dfd42c01d42575c3f81bc36f4bc65
0obito <elmsafer.abdellah00@gmail.com>
2023-10-11 01:34:39 +0100

    'Bingo'

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees everything
+ *
+ * @d: dog's profile.
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+
+	free(d);
+}
