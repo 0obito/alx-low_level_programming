7dafdec65aeb33851756277de4d990972cba4994
0obito <elmsafer.abdellah00@gmail.com>
2023-10-11 14:37:58 +0100

    'olazomi'

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executs a function.
+ * @array: pointer to an array.
+ * @size: size of array.
+ * @action: pointer to a function to use.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+
+
+}
