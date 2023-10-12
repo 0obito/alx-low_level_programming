6d7613bbecc395031982bf5a9e4e798ac1cac6a1
0obito <elmsafer.abdellah00@gmail.com>
2023-10-12 17:28:08 +0100

    'olazomi'

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+	
+
+}
