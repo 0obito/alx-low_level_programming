ac8217a919db8ad417cdb956d0ac04b1091a8bf6
0obito <elmsafer.abdellah00@gmail.com>
2023-10-12 18:24:18 +0100

    'olazomi'

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 
 	va_start(args, n);
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		temp = va_arg(args, int);
 		s = s + temp;
