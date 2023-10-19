bd47780279eb5a3164c8a5245cdfe13f1c30d309
0obito <elmsafer.abdellah00@gmail.com>
2023-10-19 22:27:09 +0100

    Went with a new approach to use malloc and free

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "lists.h"
 
@@ -12,24 +13,30 @@
 size_t print_list(const list_t *h)
 {
 	int count = 0;
+	list_t *hDup;
 
 	if (h == NULL)
-	{
-		free(h);
 		return (-1);
-	}
+
+	hDup = malloc(sizeof(list_t));
+
+	hDup->str = h->str;
+	hDup->len = h->len;
+	hDup->next = h->next;
 
 	do {
 
-	if (h->str == NULL)
+	if (hDup->str == NULL)
 		printf("[0] (nil)\n");
 	else
-		printf("[%u] %s\n", h->len, h->str);
+		printf("[%u] %s\n", hDup->len, hDup->str);
 
 	count++;
-	h = h->next;
+	hDup = hDup->next;
 
-	} while (h != NULL);
+	} while (hDup != NULL);
+
+	free(hDup);
 
 	return (count);
 }
