68c82ecde10bd9d0d6fb6da339b67b1abf8df9c8
0obito <elmsafer.abdellah00@gmail.com>
2023-10-19 17:37:35 -0700

    Identified the problem, still have to dix betty

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,48 +3,36 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/**
- * add_node_end - adds a new node at the end of a list.
- *
- * @head: memory adresse of the head.
- * @str: the name sting.
- *
- * Return: the address of the new element, or NULL if it failed.
- */
-list_t *add_node_end(list_t **head, const char *str)
-{
-	char *dup;
-	list_t *new, *temp;
+list_t *add_node_end(list_t **head, const char *str) {
+    char *dup;
+    list_t *new, *temp;
 
-	if (str == NULL)
-		return (NULL);
+    if (str == NULL)
+        return (NULL);
 
-	dup = strdup(str);
+    dup = strdup(str);
+    if (dup == NULL)
+        return (NULL);
 
-	if (dup == NULL)
-		return (NULL);
+    new = malloc(sizeof(list_t));
+    if (new == NULL) {
+        free(dup);
+        return (NULL);
+    }
 
-	new = malloc(sizeof(list_t));
+    new->str = dup;
+    new->len = strlen(dup);
+    new->next = NULL;
 
-	if (new == NULL)
-	{
-		free(dup);
-		return (NULL);
-	}
+    if (*head == NULL) {
+        *head = new; 
+    } else {
+        temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new;
+    }
 
-	new->str = dup;
-	new->len = strlen(dup);
-	new->next = NULL;
-
-	if (*head == NULL)
-		*head = new;
-
-	temp = *head;
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	temp->next = new;
-
-	return (new);
+    return (new);
 }
