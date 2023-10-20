beb2a7cdaea8452432d460e09d71b7d8b429c7b2
0obito <elmsafer.abdellah00@gmail.com>
2023-10-20 02:10:58 +0100

    Fixed betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_list - frees a list_t list.
+ *
+ * @head: pointer to the head of the list.
+ */
 void free_list(list_t *head)
 {
 	list_t *current;
