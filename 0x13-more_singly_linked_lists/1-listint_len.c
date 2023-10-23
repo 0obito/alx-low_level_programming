db4b10dc353f15f4294dec540c98022dbf35e54d
0obito <elmsafer.abdellah00@gmail.com>
2023-10-23 17:29:30 +0100

    Made task_1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,14 +3,14 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements
- * of a listint_t list
+ * listint_len - counts the number of
+ * elements in a linked list
  *
  * @h: list head
  *
- * Return: number of nodes
+ * Return: number of elements
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	int count = 0;
 
