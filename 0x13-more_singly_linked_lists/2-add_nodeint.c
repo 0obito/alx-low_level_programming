c89c5726863d4782fea419fec55124b380e574e8
0obito <elmsafer.abdellah00@gmail.com>
2023-10-23 17:51:55 +0100

    Dealt with betty for task_2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,7 +6,7 @@
  * add_nodeint - adds a new node
  * at the beginning of a listint_t list
  *
- * @h: list head
+ * @head: list head
  * @n: the new node int
  *
  * Return: address of the new element
@@ -16,10 +16,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *nuevo;
 
 	nuevo = malloc(sizeof(listint_t));
-
 	if (nuevo == NULL)
 		return (NULL);
-
 	nuevo->n = n;
 	nuevo->next = *head;
 	*head = nuevo;
