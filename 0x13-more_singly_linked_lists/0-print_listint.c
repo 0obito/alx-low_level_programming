1aa929470a76fd4b9dfa2076de5f3077db12e87c
0obito <elmsafer.abdellah00@gmail.com>
2023-10-23 17:06:19 +0100

    Made task_0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h
+#include <stdlib.h>
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return(count);
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return(count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+"This is the README.md file for more singly linked lists project" 
