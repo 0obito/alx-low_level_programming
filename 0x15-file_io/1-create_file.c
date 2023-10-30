5d119180a4f5622b0ed9c2d4cdad00c6515d2df3
0obito <elmsafer.abdellah00@gmail.com>
2023-10-30 22:52:21 +0100

    Made some changes in task 01

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_APPEND);
+	fd = open(filename, O_CREAT | O_RDONLY | O_TRUNC);
 	if (fd == -1)
 		return (-1);
 
