318943c0372bba2c2dfac6b27ecae158b7d4d04c
0obito <elmsafer.abdellah00@gmail.com>
2023-12-22 20:37:40 +0100

    3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,54 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_t - a function that adds an element to the hash table.
+ *
+ * @value: the value associated with the key.
+ * @key: the key.
+ * @ht: the hash table we want to update.
+ *
+ * Return: 1 in success, 0 in failure.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index, size;
+	char *buffer, *cle;
+	hash_node_t *node, *temp = NULL;
+
+	if (key == NULL || value == NULL || ht == NULL)
+		return (0);
+
+	size = ht->size;
+	index = key_index((const unsigned char *)key, size);
+
+	buffer = strdup(value);
+	if (buffer == NULL)
+		return (0);
+	cle = strdup(key);
+	if (cle == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (temp == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	node->key = cle;
+	node->value = buffer;
+	node->next = NULL;
+
+	if (ht->array[index] == NULL)
+		ht->array[index] = node;
+	else
+	{
+		temp = ht->array[index];
+		ht->array[index] = node;
+		ht->array[index]->next = temp;
+	}
+
+	return (1);
+}
