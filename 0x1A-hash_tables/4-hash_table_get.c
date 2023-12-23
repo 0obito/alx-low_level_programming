96b72175d906dcc8c0c3dbdb213452fbf022673d
0obito <elmsafer.abdellah00@gmail.com>
2023-12-23 01:46:04 +0100

    4~

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,17 +1,22 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - a function that retrieves a value associated with a key.
+ * hash_table_get - a function that
+ * retrieves a value associated with a key.
  *
  * @ht: the hash table we want to update.
  * @key: the key.
  *
- * Return: the value associated with the element, or NULL if key couldn’t be found.
+ * Return: the value associated with the element,
+ * or NULL if key couldn’t be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index, size;
 	hash_node_t *temp;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
 
 	size = ht->size;
 	index = key_index((const unsigned char *)key, size);
